0485625d0a63934a520c05d817135ac896c4b231
jome95 <jmbv.dev@gmail.com>
2023-06-27 02:51:47 +0200

    Fetching restaurants details data

--- app/components/Price/Price.tsx
+++ app/components/Price/Price.tsx
@@ -4,17 +4,19 @@ import React from "react";
 const Price = ({ price }: { price: PRICE }) => {
   const renderPrice = () => {
     if (price === PRICE.CHEAP) {
-      return;
-      <>
-        <span>$$</span>
-        <span className="text-gray-400">$$</span>
-      </>;
+      return (
+        <>
+          <span>$$</span>
+          <span className="text-gray-400">$$</span>
+        </>
+      );
     } else if (price === PRICE.REGULAR) {
-      return;
-      <>
-        <span>$$$</span>
-        <span className="text-gray-400">$</span>
-      </>;
+      return (
+        <>
+          <span>$$$</span>
+          <span className="text-gray-400">$</span>
+        </>
+      );
     } else {
       return <span className="text-gray-400">$$$$</span>;
     }
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -5,9 +5,40 @@ import RestaurantNavBar from "@/app/components/restaurant/RestaurantNavBar/Resta
 import RestaurantRating from "@/app/components/restaurant/RestaurantRating/RestaurantRating";
 import RestaurantReviews from "@/app/components/restaurant/RestaurantReviews/RestaurantReviews";
 import RestaurantTitle from "@/app/components/restaurant/RestaurantTitle/RestaurantTitle";
+import { PrismaClient } from "@prisma/client";
 import React from "react";
 
-const RestaurantDetail = () => {
+const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: number;
+  name: string;
+  description: string;
+  images: string[];
+  slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      images: true,
+      slug: true,
+    },
+  });
+  if (!restaurant) {
+    throw new Error();
+  }
+  return restaurant;
+};
+
+const RestaurantDetail = async ({ params }: { params: { slug: string } }) => {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
