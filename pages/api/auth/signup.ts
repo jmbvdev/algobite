9842012c518042d2192880a14aa98143016e2506
jome95 <jmbv.dev@gmail.com>
2023-06-30 18:28:56 +0200

    Signup http request

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,102 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import * as jose from "jose";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { firstName, lastName, password, city, email, phone } = req.body;
+
+    const errors: string[] = [];
+
+    const validationSchema = [
+      {
+        valid: validator.isLength(firstName, {
+          min: 2,
+          max: 30,
+        }),
+        errorMessage: "Please enter a valid name",
+      },
+      {
+        valid: validator.isLength(lastName, {
+          min: 2,
+          max: 30,
+        }),
+        errorMessage: "Please enter a valid last name",
+      },
+      {
+        valid: validator.isEmail(email),
+        errorMessage: "Email is invalid",
+      },
+      {
+        valid: validator.isMobilePhone(phone),
+        errorMessage: "The phone number is invalid",
+      },
+      {
+        valid: validator.isLength(city, {
+          min: 1,
+        }),
+        errorMessage: "City is invalid",
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        errorMessage:
+          "Please use a mix of letters (uppercase and lowercase), numbers, and symbols.",
+      },
+    ];
+
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.errorMessage);
+      }
+    });
+
+    if (errors.length) {
+      return res.status(400).json({ errorMessage: errors[0] });
+    }
+
+    const userWithEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithEmail) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Email is associated with another account" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        password: hashedPassword,
+        city,
+        phone,
+        email,
+      },
+    });
+
+    const alg = "HS256";
+
+    const secret= new TextEncoder().encode(process.env.JWT_SECRET)
+
+    const token = await new jose.SignJWT({ email: user.email })
+      .setProtectedHeader({ alg })
+      .setExpirationTime("24h")
+      .sign(secret);
+
+    res.status(200).json({
+      hello: "body",
+    });
+  }
+}
